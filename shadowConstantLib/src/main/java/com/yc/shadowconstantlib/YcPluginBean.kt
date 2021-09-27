1fa16b4e20ec5023245fff6b1fd6efe62139e505
yc <563919364@qq.com>
2021-09-27 17:59:30 +0800

    bundle转bean

--- shadowConstantLib/src/main/java/com/yc/shadowconstantlib/YcPluginBean.kt
+++ shadowConstantLib/src/main/java/com/yc/shadowconstantlib/YcPluginBean.kt
@@ -1,6 +1,7 @@
 package com.yc.shadowconstantlib
 
 import android.os.Bundle
+import kotlin.jvm.Throws
 
 /**
  * Creator: yc
@@ -19,9 +20,36 @@ data class YcPluginBean(
     val pluginZipPath: String,
     val partKey: String,
     val startActivityName: String,
-    val dataBundle: Bundle = Bundle(),
+    val dataBundle: Bundle? = null,
     val extraPlugin: ExtraPlugin? = null,
 ) {
+    companion object {
+        @Throws(Throwable::class)
+        fun toBean(bundle: Bundle): YcPluginBean {
+            val serviceName = bundle.getString(YcHostManager.KEY_PPS_SERVICE_NAME) ?: throw Throwable("pps服务为空")
+            val pluginZipPath = bundle.getString(YcHostManager.KEY_PLUGIN_ZIP_PATH) ?: throw Throwable("pluginZipPath路径为空")
+            val pluginPartKey = bundle.getString(YcHostManager.KEY_PLUGIN_PART_KEY) ?: throw Throwable("插件的PartKey为空")
+            val activityClassName = bundle.getString(YcHostManager.KEY_PLUGIN_START_ACTIVITY_CLASSNAME) ?: throw Throwable("插件的启动的Activity类为空")
+            val dataBundle = bundle.getBundle(YcHostManager.KEY_PLUGIN_DATA_BUNDLE)
+            //是否有额外插件
+            val isContainsExtra = bundle.getBoolean(YcHostManager.KEY_PLUGIN_EXTRA_IS_CONTAINS, false)
+
+            return if (isContainsExtra) {
+                val extraDataApkPath = bundle.getString(YcHostManager.KEY_PLUGIN_EXTRA_APK_PATH) ?: throw Throwable("额外插件的Apk路径为空")
+                val extraDataBusinessName = bundle.getString(YcHostManager.KEY_PLUGIN_EXTRA_BUSINESS_NAME) ?: pluginPartKey
+                val extraDataHash = bundle.getString(YcHostManager.KEY_PLUGIN_EXTRA_DATA_HASH) ?: throw Throwable("额外插件的Hash为空")
+                val extraIsUpgrade = bundle.getBoolean(YcHostManager.KEY_PLUGIN_EXTRA_IS_UPGRADE, false)
+                YcPluginBean(
+                    serviceName, pluginZipPath, pluginPartKey, activityClassName, dataBundle, ExtraPlugin(
+                        extraDataApkPath, extraDataHash, extraIsUpgrade, extraDataBusinessName
+                    )
+                )
+            } else {
+                YcPluginBean(serviceName, pluginZipPath, pluginPartKey, activityClassName, dataBundle, null)
+            }
+        }
+    }
+
     /**
      * 额外的插件（不在zip包里）
      * @property extraApkPath String        插件apk路径
@@ -55,4 +83,5 @@ data class YcPluginBean(
             }
         }
     }
+
 }
